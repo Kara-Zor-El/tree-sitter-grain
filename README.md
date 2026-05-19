f94a4e40e803364c14b972d88ac9691bcee9b49d
Spotandjake <spotandjake@hotmail.com>
2026-05-18 20:09:16 -0400

    feat: formatter

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ In order to build the project you can run:
 
 ```sh
 npm run build
-``` 
+```
 
 or if you want to generate the wasm bindings:
 
@@ -28,4 +28,4 @@ A testing script has been setup in `test.mjs`, the tests clone the grain, repo a
 
 The tests can be run using `npm run test:validate`, it's best to run them locally as they must pass before merging, by default the tests are run against the `main` branch of grain however if you want to run them against a specific version you can run `npm run test:validate --commit=grain-v0.7.2` for example.
 
-As a note this is a quick test of the validity of the parser and ensures that new syntax being introduced and consumed in the stdlib parses correctly however it does not test that the parse tree is being formed correctly or that the grammar aligns perfectly with grains. 
+As a note this is a quick test of the validity of the parser and ensures that new syntax being introduced and consumed in the stdlib parses correctly however it does not test that the parse tree is being formed correctly or that the grammar aligns perfectly with grains.
--- test.mjs
+++ test.mjs
@@ -1,8 +1,8 @@
-import { exec } from 'node:child_process';
-import { parseArgs } from 'node:util';
+import { exec } from "node:child_process";
+import { parseArgs } from "node:util";
 import { styleText } from "node:util";
-import path from 'node:path';
-import fs from 'node:fs/promises';
+import path from "node:path";
+import fs from "node:fs/promises";
 import test from "node:test";
 import assert from "node:assert";
 
@@ -34,7 +34,8 @@ const runGit = (args) => {
     });
   });
 };
-const hasTreeSitterParsingErrors = (source) => parser.parse(source).rootNode.hasError;
+const hasTreeSitterParsingErrors = (source) =>
+  parser.parse(source).rootNode.hasError;
 const error = (msg) => console.error(styleText(["red", "bold"], "Error:"), msg);
 const info = (msg) => console.log(styleText(["blue", "bold"], "Info:"), msg);
 
@@ -44,31 +45,39 @@ const { values, positionals } = parseArgs({
   options: {
     commit: {
       type: "string",
-      default: "main"
+      default: "main",
     },
     debug: {
       type: "boolean",
       default: false,
-    }
-  }
+    },
+  },
 });
 
 // Clone the grain repo
 info(`Cloning \`${GRAIN_REPO_URL}\` repo at \`${values.commit}\``);
 await fs.rm("./grain", { recursive: true, force: true });
-await runGit(["clone", GRAIN_REPO_URL]).catch(e => {
+await runGit(["clone", GRAIN_REPO_URL]).catch((e) => {
   error(`Failed to clone grain repo from \`${GRAIN_REPO_URL}\``);
-  error("Please make sure you have access to the repo and that the URL is correct.");
+  error(
+    "Please make sure you have access to the repo and that the URL is correct.",
+  );
   error("`--debug` can be used to get more information about the error.");
   if (values.debug) throw new Error(`Failed to clone grain repo: ${e.message}`);
   process.exit(1);
 });
-await runGit(["-C", "grain", "checkout", values.commit]).catch(e => {
+await runGit(["-C", "grain", "checkout", values.commit]).catch((e) => {
   error(`Failed to checkout commit \`${values.commit}\``);
   error("Please make sure the commit exists and is accessible.");
-  error("If you are trying to test against the latest preview use `--commit=main`.");
-  error("If you are trying to test against a specific version use `--commit=grain-vX.Y.Z`.");
-  error("If you are trying to test against a specific commit use `--commit=COMMIT_HASH`.");
+  error(
+    "If you are trying to test against the latest preview use `--commit=main`.",
+  );
+  error(
+    "If you are trying to test against a specific version use `--commit=grain-vX.Y.Z`.",
+  );
+  error(
+    "If you are trying to test against a specific commit use `--commit=COMMIT_HASH`.",
+  );
   error("`--debug` can be used to get more information about the error.");
   if (values.debug) throw new Error(`Failed to clone grain repo: ${e.message}`);
   process.exit(1);
@@ -79,7 +88,10 @@ test("Stdlib", async (t) => {
   for await (const file of getGrainFiles("./grain/stdlib")) {
     await t.test(`Testing \`${file}\``, async () => {
       const source = await fs.readFile(file, "utf8");
-      assert.ok(!hasTreeSitterParsingErrors(source), `Tree-sitter failed to parse \`${file}\`, for more information investigate the error using the tree-sitter CLI.`);
+      assert.ok(
+        !hasTreeSitterParsingErrors(source),
+        `Tree-sitter failed to parse \`${file}\`, for more information investigate the error using the tree-sitter CLI.`,
+      );
     });
   }
-})
+});
